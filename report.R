447ff4513e5dec38c552fdb5a3bb7de81235a746
Colin Millar <colin.millar@ices.dk>
2023-06-07 12:16:58 +0200

    add another check plot

--- report.R
+++ report.R
@@ -6,6 +6,7 @@
 library(icesTAF)
 library(ggplot2)
 library(sf)
+library(dplyr)
 
 mkdir("report")
 
@@ -43,3 +44,15 @@ ggplot() +
   geom_sf(data = vms_sum, aes(fill = unique_vessels), col = NA) +
   geom_sf(data = eez, aes(), fill = NA)
 ggsave("check_unique_vessels.png", path = "report", dpi = 900, width = 9, height = 9)
+
+# table data
+
+ggplot(
+  wgcran_vms %>%
+    tibble() %>%
+    count(month, year),
+    aes(month, n, colour = factor(year))
+  ) +
+  geom_line() +
+  ggtitle("C-squares fished by month and year")
+ggsave("csquares_fished_timeseries.png", path = "report", dpi = 900, width = 9, height = 9)
